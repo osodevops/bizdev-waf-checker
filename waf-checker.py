e0766832125750224534fdd19b4f44a8229e9f93
Sion Smith <sion.smith@gmail.com>
2019-12-30 12:05:15 +0000

    updates to improve i/o of scripts.

--- waf-checker.py
+++ waf-checker.py
@@ -34,27 +34,22 @@ def get_logger(args):
     """
     # log level
     log_level = logging.CRITICAL
-    # if args['--verbose'] or args['check']:
-    #     log_level = logging.INFO
-    # if args['--debug']:
-    #     log_level = logging.DEBUG
-    #
-    # log_format = '%(name)s: %(levelname)-9s%(message)s'
-    # if args['--debug']:
-    #     log_format = '%(name)s: %(levelname)-9s%(funcName)s():  %(message)s'
-    # if (('--export-json' in args and not args['--export-json']) and not args['check']):
-    #     log_format = '[dryrun] %s' % log_format
+    if args['--verbose'] or args['inspect']:
+        log_level = logging.INFO
+    if args['--debug']:
+        log_level = logging.DEBUG
 
-    # logFormatter = logging.Formatter(log_format)
+    log_format = '%(name)s: %(levelname)-9s%(message)s'
+    if args['--debug']:
+        log_format = '%(name)s: %(levelname)-9s%(funcName)s():  %(message)s'
+
+    logFormatter = logging.Formatter(log_format)
     rootLogger = logging.getLogger()
     rootLogger.setLevel(log_level)
-
     #add console appened
     consoleHandler = logging.StreamHandler()
-    # consoleHandler.setFormatter(logFormatter)
-
+    consoleHandler.setFormatter(logFormatter)
     rootLogger.addHandler(consoleHandler)
-
     return rootLogger
 
 def load_websites_from_file(log, file_name):
@@ -64,6 +59,34 @@ def load_websites_from_file(log, file_name):
     log.info("Found: %s webites to check." %(len(websites)))
     return websites
 
+
+def run_wafw00f_container_against_targets(log, client, website_list):
+    try:
+        #setup docker using easy interfact
+        host_dir = os.getcwd()
+        container_dir = '/tmp'
+        log.info("Setting Docker volume local mount to: %s" %(host_dir))
+
+        #build WAFW00F argument string
+        log.info("Building Docker args string with %s targets." %(len(website_list)))
+        targets = [ item['url'] for item in website_list ]
+        wafw00f_docker_args = ' '.join(targets) + ' --output /tmp/out.json'
+        log.debug("Docker args: %s" %(wafw00f_docker_args))
+
+        container = client.containers.run(
+            'osodevops/wafw00f:latest',
+            wafw00f_docker_args,
+            volumes = {host_dir:
+               {'bind': container_dir, 'mode': 'rw'}},
+            detach=False,
+            auto_remove=True,
+            environment={})
+        # for line in container.logs(stream=True):
+        #     print(line.decode("utf-8").strip())
+    except:
+        log.error("Socket error: failed to connect to Docker %s " % (client))
+
+
 def main():
     args = docopt(__doc__, version='1.0')
     log = get_logger(args)
@@ -72,27 +95,12 @@ def main():
     log.info("Loading website urls...")
     website_list = load_websites_from_file(log, 'in.json')
 
+    #initialise Docker client
+    log.info("Initialising Docker client...")
     client = docker.from_env()
-    env_vars = {}
 
-    # setup docker using easy interfact
-    host_dir = os.getcwd()
-    container_dir = '/tmp'
-
-    targets = [ item['url'] for item in website_list ]
-    wafw00f_docker_args = ' '.join(targets) + ' --output /tmp/out.json'
-
-    container = client.containers.run(
-        'osodevops/wafw00f:latest',
-        wafw00f_docker_args,
-        volumes = {host_dir:
-           {'bind': container_dir, 'mode': 'rw'}},
-        detach=True,
-        auto_remove=True,
-        environment=env_vars)
-    for line in container.logs(stream=True):
-        print(line.decode("utf-8").strip())
-
+    #run WAFW00F against targets
+    run_wafw00f_container_against_targets(log, client, website_list)
 
 if __name__ == "__main__":
     main()
